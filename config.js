dfd0d94529e54855a80788cf7a2abb5f191c9eac
Evan Plaice <evanplaice@gmail.com>
2016-01-24 01:51:29 -0800

    Install zone.js

--- config.js
+++ config.js
@@ -17,6 +17,7 @@ System.config({
     "reflect-metadata": "npm:reflect-metadata@0.1.3",
     "traceur": "github:jmcriffey/bower-traceur@0.0.92",
     "traceur-runtime": "github:jmcriffey/bower-traceur-runtime@0.0.92",
+    "zone.js": "npm:zone.js@0.5.10",
     "github:jspm/nodelibs-assert@0.1.0": {
       "assert": "npm:assert@1.3.0"
     },
